db3ba33c803c2f7aa1a83a22773df3e1eeb1c474
sv101 <70589012+sv101@users.noreply.github.com>
2022-06-24 23:37:58 -0400

    updates

--- pages/enter.js
+++ pages/enter.js
@@ -1,17 +1,36 @@
+import { async } from '@firebase/util'
+import { auth, googleAuthProvider } from '../lib/firebase'
+
 export default function EnterPage({ }) {
+    const user = null
+    const username = null
+
     return (
         <main>
-            <h1>Sign Up</h1>
+            {user ?
+                !username ? <UsernameForm /> : <SignOutButton />
+                :
+                <SignInButton />
+            }
+    
         </main>
     )
 }
 
 function SignInButton(){
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider)
+    }
 
+    return (
+        <buton className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} /> Sign in with Google
+        </buton>
+    )
 }
 
 function SignOutButton(){
-
+    return <button onClick={() => auth.signOut()}>Sign Out</button>
 }
 
 function UsernameForm(){
